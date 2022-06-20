707f9f3a95c89d4c9a8a5e7f5eb36d016fc9a4a9
PupperParty <93052348+PupperParty@users.noreply.github.com>
2022-06-20 12:00:38 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -8,6 +8,22 @@ function setup(){
     video = createCapture(VIDEO);
     video.size(300, 300);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x =" + results[0].pose.nose.x);
+        console.log("nose y =" + results[0].pose.nose.y);
+    }
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Initialized');
 }
 
 function draw(){
